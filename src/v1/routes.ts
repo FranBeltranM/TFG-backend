c735458e344bb847fbde5b18f5fb3e8837ecc164
becario-emtusa <contacto@franbeltran.es>
2024-06-02 17:59:07 +0200

    🔨 Fix

--- src/v1/routes.ts
+++ src/v1/routes.ts
@@ -1,4 +1,4 @@
-import { routesBrandModel, routesVehicle } from '@/v1/app/api/infrastructure/api-routes'
+import { routesBrandModel, routesConstants, routesVehicle } from '@/v1/app/api/infrastructure/api-routes'
 import { routesDefault } from '@/v1/app/default/default.routes'
 
-export const routesV1 = [routesDefault, routesVehicle, routesBrandModel]
+export const routesV1 = [routesDefault, routesVehicle, routesBrandModel, routesConstants]
